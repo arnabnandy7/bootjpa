65dc88920d91990d83d2324deccbc2171db50e5e
Arnab Nandy <arnab_nandy7@yahoo.com>
2020-05-08 12:16:45 +0530

    Added swagger documentation to project for RESTful Webservices

--- src/main/java/com/arnab/demo/BootjpaApplication.java
+++ src/main/java/com/arnab/demo/BootjpaApplication.java
@@ -2,8 +2,16 @@ package com.arnab.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BootjpaApplication {
 	// To check H2 database connect with http://localhost:8080/h2-console/
 	// Change the JDBC URL with jdbc:h2:mem:arnabjpa
@@ -11,6 +19,17 @@ public class BootjpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootjpaApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.arnab.demo.controller")) // // Generate API of
+																						// EndPoints which is
+																						// available inside
+																						// defined package
+				.paths(PathSelectors.any()) // for all EndPoints
+				.build(); // create object
 	}
 
 }
--- src/main/java/com/arnab/demo/controller/AlienController.java
+++ src/main/java/com/arnab/demo/controller/AlienController.java
@@ -20,6 +20,8 @@ import org.springframework.web.servlet.ModelAndView;
 import com.arnab.demo.Model.Alien;
 import com.arnab.demo.dao.AlienRepo;
 
+import io.swagger.annotations.ApiOperation;
+
 /**
  * 
  * @author arnab
@@ -31,8 +33,7 @@ import com.arnab.demo.dao.AlienRepo;
  *        method.
  *
  */
-//@Controller
-@RestController
+@Controller
 public class AlienController {
 	@Autowired
 	AlienRepo repo;
@@ -98,83 +99,5 @@ public class AlienController {
 		System.out.println(repo.findByAidGreaterThan(105));
 		System.out.println(repo.findByTechSorted(tech));
 		return mv;
-	}
-
-	// REST Responses
-
-	/**
-	 * This will call when client hits following link http://localhost:8080/aliens
-	 * 
-	 * In order to restrict to a type you have to specify accepted type in produces
-	 * to Request Mapping as produces={"application/xml"}
-	 * 
-	 * For content negotiations you have to add xml support to pom.xml to add
-	 * jackson dataformat xml from maven repository
-	 * 
-	 * @return List<Alien>
-	 */
-	// @RequestMapping(path = "/aliens", produces = { "application/xml" })
-	@GetMapping("/aliens")
-	// @ResponseBody
-	public List<Alien> getAliens() {
-		return repo.findAll();
-	}
-
-	/**
-	 * This will call when client hits following link
-	 * http://localhost:8080/alien/103
-	 * 
-	 * @param aid
-	 * @return Optional<Alien>
-	 */
-	@GetMapping("/alien/{aid}")
-	// @ResponseBody
-	public Optional<Alien> getAlienById(@PathVariable("aid") int aid) {
-		return repo.findById(aid);
-	}
-
-	/**
-	 * This will call when client want to add an alien
-	 * 
-	 * Like produces we can also restrict consumes which will ensure only specified
-	 * type of data to be accepted by server using consumes = { "application/json" }
-	 * 
-	 * @param alien
-	 * @return Alien
-	 */
-	// @PostMapping("/alien")
-	@PostMapping(path = "/alien", consumes = { "application/json" })
-	public Alien addAlienByRest(@RequestBody Alien alien) {
-		repo.save(alien);
-		return alien;
-	}
-
-	/**
-	 * This will call when client want to delete an alien
-	 * 
-	 * @param aid
-	 * @return Optional<Alien>
-	 */
-	@DeleteMapping("/alien/{aid}")
-	public Optional<Alien> deleteAlien(@PathVariable("aid") int aid) {
-		Optional<Alien> alien = repo.findById(aid);
-		repo.deleteById(aid);
-		return alien;
-	}
-
-	@PutMapping("/alien")
-	public Alien updateAlien(@RequestBody Alien alien) {
-		Optional<Alien> alienOrig = repo.findById(alien.getAid());
-		Alien alteredAlien;
-		if (alienOrig.isPresent()) {
-			alteredAlien = alienOrig.get();
-			alteredAlien.setAid(alien.getAid());
-			alteredAlien.setAname(alien.getAname());
-			alteredAlien.setTech(alien.getTech());
-			repo.save(alteredAlien);
-		} else {
-			alteredAlien = null;
-		}
-		return alteredAlien;
 	}
 }
