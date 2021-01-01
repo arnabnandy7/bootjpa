2e21f587037e77718d70fbcd3a92cbff8939e892
Arnab Nandy <arnab_nandy7@yahoo.com>
2021-01-01 19:51:14 +0530

    Fixed build failure issue

--- src/main/java/com/arnab/demo/BootjpaApplication.java
+++ src/main/java/com/arnab/demo/BootjpaApplication.java
@@ -2,17 +2,9 @@ package com.arnab.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class BootjpaApplication {
@@ -21,23 +13,10 @@ public class BootjpaApplication {
 	// Press connect, DONOT CHANGE ANY OTHER SETTINGS
 	
 	private static final Logger logger = LoggerFactory.getLogger(BootjpaApplication.class);
-	
-	@Autowired
-	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootjpaApplication.class, args);
-	}
-
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.arnab.demo.Controller")) // // Generate API of
-																						// EndPoints which is
-																						// available inside
-																						// defined package
-				.paths(PathSelectors.any()) // for all EndPoints
-				.build(); // create object
+		logger.info("Application initiated");
 	}
 
 }
